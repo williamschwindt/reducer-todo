be2a07750223d5a35a13bc6126734df3e65ddec1
William Schwindt <williammschwindt@gmail.com>
2020-02-26 16:15:25 -0800

    initial state set up, basic reducer

---
+++ todo-list/src/reducers/index.js
@@ -0,0 +1,23 @@
+export const initialList = [
+    {
+        item: 'learn about reducers',
+        completed: false,
+        id: 3892987589
+    },
+
+    {
+        item: 'learn redux',
+        completed: false,
+        id: 3892987590
+    },
+
+    {
+        item: 'learn async redux',
+        completed: false,
+        id: 3892987591
+    }
+];
+
+export const listReducer = (state, action) => {
+    return state;
+}
